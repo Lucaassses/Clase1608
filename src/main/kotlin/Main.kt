9bfe076d8aaf8eccd6115a9f64d9d72b7dd157dc
zip-b104-e-005 <zip-b104-e-005@UMD.LOCAL.EDU>
2024-08-16 15:43:20 -0500

    Monedas

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,30 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import kotlin.random.Random
 
-    println("Program arguments: ${args.joinToString()}")
+fun main() {
+
+    var aleatorio : Int
+    var cara = 0
+    var sello = 0
+
+    for (i in 1..20){
+        aleatorio = Random.nextInt(2)
+
+        if (aleatorio == 0)
+            cara ++
+        else if(aleatorio == 1)
+            sello ++
+
+    }
+    println("Veces caidas en cara: $cara")
+    println("Veces caidas en sello: $sello")
+
+
+
+
+
+
+
+
+
+
 }
